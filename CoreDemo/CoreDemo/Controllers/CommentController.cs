c272c182071ecea83c5a790b693432efabb388ac
Petronerd1 <bedirhanttepe@gmail.com>
2021-11-19 16:59:49 +0300

    Blog1.5
    Blog1.5

---
+++ CoreDemo/CoreDemo/Controllers/CommentController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager cm = new CommentManager(new EFCommentRepository());
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialAddComment()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialListByBlog(int id)
+        {
+            var value =cm.GetList(id);
+            return PartialView(value);
+
+        }
+    }
+}
